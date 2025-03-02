4849b3a8f03e64b03f10382ebc19374bb52e0ed6
Jananporn Banjob <jananporn.banjob.gamil.com>
2025-03-02 15:27:15 +0700

    add Alternating_character

---
+++ test/test_alternating_characters.py
@@ -0,0 +1,26 @@
+
+import unittest
+from coe_number.alternating_characters import alternatingCharacters
+
+class TestAlternatingCharacters(unittest.TestCase):
+
+    def test_alternating_characters(self):
+
+        self.assertEqual(alternatingCharacters("ABABAB"), 0)
+
+        self.assertEqual(alternatingCharacters("AABBAA"), 4)
+
+        self.assertEqual(alternatingCharacters("AAAA"), 3)
+
+        self.assertEqual(alternatingCharacters("A"), 0)
+
+        self.assertEqual(alternatingCharacters(""), 0)
+
+        self.assertEqual(alternatingCharacters("AB"), 0)
+
+        self.assertEqual(alternatingCharacters("AABBAAAB"), 4)
+
+        self.assertEqual(alternatingCharacters("ABABABAB"), 0)
+
+
+unittest.main()
